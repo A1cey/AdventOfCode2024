333d13bfc957f4727ee663d7c7464f9b33b5f6f9
A1cey <mschelkle05@web.de>
2024-12-03 16:14:11 +0100

    day 2

---
+++ src/day2.rs
@@ -0,0 +1,47 @@
+use std::fs;
+
+pub fn run() {
+    println!("Day 2:");
+
+    let mut ans1 = 0;
+    let mut ans2 = 0;
+
+    for line in fs::read_to_string("src/input2.txt")
+        .unwrap()
+        .split("\n")
+        .collect::<Vec<&str>>()
+    {
+        let mut values: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        if is_save(&mut values) {
+            ans1 += 1;
+            ans2 += 1;
+            continue;
+        }
+
+        for i in 0..values.len() {
+            let mut vec = values.clone();
+            vec.remove(i);
+
+            if is_save(&mut vec) {
+                ans2 += 1;
+                break;
+            }
+        }
+    }
+    println!("{} reports are safe.", ans1);
+    println!("{} reports are now safe.", ans2);
+}
+
+fn is_save(values: &mut Vec<i32>) -> bool {
+    if values.first() > values.iter().nth(1) {
+        values.reverse();
+    }
+
+    values
+        .iter()
+        .is_sorted_by(|a, b| a < b && *b - *a >= 1 && *b - *a <= 3)
+}
