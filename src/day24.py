a70b4e4e5bd8c51daf6b722187632bbfd192d7b3
A1cey <mschelkle05@web.de>
2024-12-24 12:43:27 +0100

    day 24

---
+++ src/day24.py
@@ -0,0 +1,156 @@
+import sys
+from typing import Iterator, NamedTuple
+
+
+class GateExpression(NamedTuple):
+    label: str
+    position: int
+
+
+class Mismatch(NamedTuple):
+    expr1: "GateExpression | Mismatch"
+    expr2: "GateExpression | Mismatch"
+    gate: str
+    position: int
+
+
+def parse_input(lines: Iterator[str]) -> tuple[dict[str, int], list[tuple[str, str, str, str]]]:
+    inputs = {}
+    gates = []
+    part = 1
+    for line in lines:
+        line = line.strip()
+        if line == "":
+            part = 2
+            continue
+        if part == 1:
+            wire, value = line.split(": ")
+            inputs[wire] = int(value)
+        else:
+            wire1, gate, wire2, _, output = line.split()
+            gates.append((wire1, gate, wire2, output))
+    return inputs, gates
+
+
+def simulate_gates(inputs: dict, gates: list):
+    wire_values = inputs.copy()
+    waiting_z_wires = {output for _, _, _, output in gates if output.startswith("z")}
+    while waiting_z_wires:
+        for wire1, gate, wire2, output in gates:
+            if wire1 in wire_values and wire2 in wire_values:
+                w1 = wire_values[wire1]
+                w2 = wire_values[wire2]
+                if gate == "AND":
+                    result = w1 & w2
+                elif gate == "OR":
+                    result = w1 | w2
+                elif gate == "XOR":
+                    result = w1 ^ w2
+                else:
+                    raise
+                wire_values[output] = result
+                if output in waiting_z_wires:
+                    waiting_z_wires.remove(output)
+    return wire_values
+
+
+def get_output_value(wire_values: dict[str, int]) -> int:
+    result = 0
+    for wire, value in wire_values.items():
+        if value and wire.startswith("z"):
+            place = int(wire[1:])
+            result += 1 << place
+    return result
+
+
+def find_swaps(gates: list[tuple[str, str, str, str]]) -> Iterator[tuple[str, str]]:
+    while True:
+        swap = find_swap(gates)
+        if swap:
+            yield swap
+            swap_outputs(swap[0], swap[1], gates)
+        else:
+            break
+
+
+def find_swap(gates: list[tuple[str, str, str, str]]) -> tuple[str, str] |None:
+    gates_by_output = {output: (wire1, gate, wire2) for wire1, gate, wire2, output in gates}
+    expressions = {get_expr(output, gates_by_output): output for output in gates_by_output}
+    for expr, output in expressions.items():
+        expr = get_expr(output, gates_by_output)
+        if expr.position == -1 and output.startswith("z") and isinstance(expr, Mismatch):
+            expr1, expr2, gate, _ = expr
+            position = int(output[1:])
+            if gate == "XOR":
+                wire1, _, wire2 = gates_by_output[output]
+                other_wire = {expr: wire for expr, wire in zip((expr1, expr2), (wire2, wire1))}
+                if GateExpression("Carry", position) in (expr1, expr2):
+                    return (
+                        other_wire[GateExpression("Carry", position)],
+                        expressions[GateExpression("Xor", position)],
+                    )
+                if GateExpression("Xor", position) in (expr1, expr2):
+                    return (
+                        other_wire[GateExpression("Xor", position)],
+                        expressions[GateExpression("Carry", position)],
+                    )
+        elif expr.position < 0:
+            continue
+        elif  isinstance(expr, GateExpression) and expr.label == "Digit" and output != f"z{expr.position:02d}":
+            return output, f"z{expr.position:02d}"
+
+
+def get_expr(output, gates_by_output) -> GateExpression | Mismatch:
+    """
+        Digit(0) = Xor(0)
+        Digit(N) = Carry(N) XOR Xor(N)
+        Carry(1) = And(0)
+        Carry(N+1) = Partial(N) OR And(N)
+        Partial(N) = Carry(N) AND Xor(N)
+        Xor(N) = x(N) XOR y(N)
+        And(N) = x(N) AND y(N)
+    """
+    if output[0] in "xy":
+        return GateExpression(output[0], int(output[1:]))
+    wire1, gate, wire2 = gates_by_output[output]
+    expr1 = get_expr(wire1, gates_by_output)
+    expr2 = get_expr(wire2, gates_by_output)
+    if isinstance(expr1, GateExpression) and isinstance(expr2, GateExpression) and expr1.position == expr2.position:
+        labels = {expr1.label, expr2.label}
+        if labels == {"x", "y"}:
+            if gate == "XOR":
+                return GateExpression("Xor" if expr1.position > 0 else "Digit", expr1.position)
+            elif gate == "AND":
+                if expr1.position == 0:
+                    return GateExpression("Carry", 1)
+                else:
+                    return GateExpression("And", expr1.position)
+        if labels == {"Carry", "Xor"} and gate in ("AND", "XOR"):
+            return GateExpression("Partial" if gate == "AND" else "Digit", expr1.position)
+        if labels == {"Partial", "And"} and gate == "OR":
+            return GateExpression("Carry", expr1.position + 1)
+    if expr1.position < 0 or expr2.position < 0:
+        return Mismatch(expr1, expr2, gate, -2)
+    return Mismatch(expr1, expr2, gate, -1)
+
+
+def swap_outputs(output1, output2, gates: list[tuple[str, str, str, str]]) -> None:
+    for i, (wire1, gate, wire2, output) in enumerate(gates):
+        if output == output1:
+            new_output = output2
+        elif output == output2:
+            new_output = output1
+        else:
+            new_output = output
+        gates[i] = (wire1, gate, wire2, new_output)
+
+
+def main():
+    inputs, gates = parse_input(open("src/input/input24.txt"))
+    print(get_output_value(simulate_gates(inputs, gates)))
+    swap1, swap2, swap3, swap4 = find_swaps(gates)
+    print(",".join(sorted(swap1 + swap2 + swap3 + swap4)))
+
+
+if __name__ == '__main__':
+    main()
--- src/day24.rs
+++ src/day24.rs
@@ -1,7 +1,134 @@
-use std::fs;
+use std::{
+    collections::{HashMap, VecDeque},
+    fs,
+    ops::{BitAnd, BitOr, BitXor},
+};
 
 pub fn run() {
     println!("Day 24:");
+    let input = fs::read_to_string("src/input/input24.txt").unwrap();
 
+    let (wire_vals, mut operations) = parse(&input);
+    
+    let correct_res = correct_result(&wire_vals);
+    let swaps = find_swaps(&wire_vals, &mut operations, correct_res);
+    
+    let result = simulate(wire_vals, &mut operations);
+    let z_num = get_result(&result, "z");
+    
+    compare(&correct_res, &z_num);
+    
+    println!("Decimal output: {z_num}");
+   // println!("Swapped wires: {swaps}");
     println!();
+}
+
+fn compare(lhs: &isize, rhs: &isize)  {
+    let a = (0..64).rev().map(|n| lhs >> n & 1).collect::<Vec<_>>();
+    let b = (0..64).rev().map(|n| rhs >> n & 1).collect::<Vec<_>>();
+    
+    let c = a.iter().enumerate().map(|(idx, bit)| b[idx] == *bit).map(|b| match b {
+        true => 1,
+        false => 0
+    }).collect::<Vec<_>>();
+    
+    println!("{:?}", c);
+}
+
+fn find_swaps(
+    wire_vals: &HashMap<&str, i32>,
+    operations: &mut VecDeque<((&str, &str, &str), fn(i32, i32) -> i32)>,
+    correct_res: isize
+)  {
+    println!("correct result: {correct_res}");
+
+    
+    
+}
+
+fn get_result(result: &HashMap<&str, i32>, res_prefix: &str) -> isize {
+    let mut z_results = result
+        .iter()
+        .filter(|(id, _)| id.starts_with(res_prefix))
+        .collect::<Vec<_>>();
+
+    z_results.sort();
+
+    z_results
+        .iter()
+        .map(|(_, num)| **num)
+        .enumerate()
+        .fold(0, |acc, (idx, num)| {
+            acc + 2_isize.pow(idx as u32) * num as isize
+        })
+}
+
+fn simulate<'a>(
+    mut wire_vals: HashMap<&'a str, i32>,
+    operations: &mut VecDeque<((&'a str, &'a str, &'a str), fn(i32, i32) -> i32)>,
+) -> HashMap<&'a str, i32> {
+    while let Some(((a, b, res), op)) = operations.pop_front() {
+        if let Some(val_a) = wire_vals.get(a) {
+            if let Some(val_b) = wire_vals.get(b) {
+                wire_vals.insert(res, op(*val_a, *val_b));
+                continue;
+            }
+        }
+
+        operations.push_back(((a, b, res), op));
+    }
+
+    wire_vals
+}
+
+fn parse(
+    input: &str,
+) -> (
+    HashMap<&str, i32>,
+    VecDeque<((&str, &str, &str), fn(i32, i32) -> i32)>,
+) {
+    let (initial, connections) = input.split_once("\r\n\r\n").unwrap();
+
+    let wire_vals = initial.lines().fold(HashMap::new(), |mut acc, line| {
+        let (id, val) = line.split_once(": ").unwrap();
+        acc.insert(id, val.parse().unwrap());
+        acc
+    });
+
+    let operations = connections
+        .lines()
+        .map(|line| {
+            let (op, res) = line.split_once(" -> ").unwrap();
+
+            let args = op.split(" ").collect::<Vec<_>>();
+            let fun = match args[1] {
+                "AND" => and,
+                "OR" => or,
+                "XOR" => xor,
+                _ => unreachable!(),
+            };
+
+            ((args[0], args[2], res), fun)
+        })
+        .collect();
+
+    (wire_vals, operations)
+}
+
+fn and(lhs: i32, rhs: i32) -> i32 {
+    lhs.bitand(rhs)
+}
+
+fn or(lhs: i32, rhs: i32) -> i32 {
+    lhs.bitor(rhs)
+}
+
+fn xor(lhs: i32, rhs: i32) -> i32 {
+    lhs.bitxor(rhs)
+}
+
+fn correct_result(wire_vals: &HashMap<&str, i32>) -> isize {
+    let (x, y) = wire_vals.iter().partition(|(id, _)| id.starts_with("x"));
+
+    get_result(&x, "x") + get_result(&y, "y")
 }
